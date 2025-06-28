2e59638af664e5dabaa08151f26b6a9a75fd006f
Piotr Jucha <piotrek1453@gmail.com>
2025-06-28 13:49:29 +0200

    playing with navbar styling, placeholders for endpoints added

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Repo with code of my Leptos+Axum personal webpage.
 
-Project created with start-axum template.
+## Useful resources
+
+- [start-axum project template](https://github.com/leptos-rs/start-axum)
+- [Setting up tailwind4 and daisyUI5 in Leptos 0.8](https://8vi.cat/leptos-0-8-tailwind4-daisyui5-for-easy-websites/)
---
+++ src/components/common/bars.rs
@@ -0,0 +1,29 @@
+use leptos::prelude::*;
+
+/// Documentation for [`NavBar`]
+#[component]
+pub fn NavBar() -> impl IntoView {
+    let nav_items = [
+        ("Home", "/"),
+        ("Projects", "/projects"),
+        ("Blog", "/blog"),
+        ("About me", "/about"),
+        ("Contact", "/contact"),
+    ];
+
+    view! {<div class="navbar rounded-2xl shadow-sm bg-gray-100 dark:bg-gradient-to-br dark: bg-linear-to-b dark:from-purple-600 dark:via-blue-500 dark:to-gray-600 dark:border-amber-600 border-1">
+        <div class="navbar-start">
+          <a class="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal px-1">
+            {nav_items.iter().map(|(label, href)| view! {
+              <li>
+                <a class="btn-ghost rounded" href={*href}>{*label}</a>
+              </li>
+            }).collect_view()}
+          </ul>
+        </div>
+      </div>
+    }
+}
