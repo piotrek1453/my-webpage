26bb299c295f0b89f1d5e567e29309d0a3e23cc7
Piotr <piotrek1453@gmail.com>
2025-10-04 10:54:37 +0200

    added wee_alloc for bundle size reduction

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@ pub mod components;
 pub mod models;
 pub mod utils;
 
+#[cfg(target_arch = "wasm32")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
