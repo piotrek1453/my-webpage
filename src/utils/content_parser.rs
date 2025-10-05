9f29535643981979d06a47f4e8fe491030d4a75e
Piotr Jucha <juchap68@gmail.com>
2025-10-05 14:49:12 +0200

    remove reading md from file: everything will be either in db or embedded in binary

--- src/utils/content_parser.rs
+++ src/utils/content_parser.rs
@@ -1,5 +1,4 @@
 use leptos::prelude::*;
-use std::fs;
 
 #[server]
 pub async fn parse_markdown(md: String) -> Result<String, ServerFnError> {
@@ -9,8 +8,4 @@ pub async fn parse_markdown(md: String) -> Result<String, ServerFnError> {
     pulldown_cmark::html::push_html(&mut html_output, parser);
 
     Ok(html_output)
-}
-
-pub fn read_markdown_from_file(md_path: String) -> String {
-    fs::read_to_string(md_path).expect("Unable to read the file")
 }
