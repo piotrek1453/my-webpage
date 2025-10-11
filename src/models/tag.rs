88bb9e69c37339dcebb2ae65ba9a6535b810b031
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2025-10-11 19:43:07 +0200

    first part of cleaning up db comms: Repository trait added and implemented for Post struct

---
+++ src/models/tag.rs
@@ -0,0 +1,8 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[cfg_attr(feature = "ssr", derive(sqlx::FromRow))]
+pub struct Tag {
+    pub id: i64,
+    pub name: String,
+}
